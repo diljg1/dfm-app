51125b3f3066a9dea7aa9a482691683a0c06cb2e
Matthijs Alles <matthijs@bixie.nl>
2020-04-14 22:07:04 +0200

    use accordion

--- src/components/ParamsForm.vue
+++ src/components/ParamsForm.vue
@@ -1,81 +1,73 @@
 <template>
 
-    <div class="uk-grid-divider uk-grid-small uk-child-width-1-3@s uk-form-stacked" uk-grid>
-        <div>
-            <h3 class="uk-flex uk-flex-middle">
-                <small class="uk-text-muted uk-margin-small-right">{{ 'Stap 1' | trans }}</small>
-                <strong class="uk-flex-1">{{ 'Data' | trans }}</strong>
-            </h3>
-            <hr class="uk-margin-small-bottom"/>
-            <Toggler v-for="(field, name) in fieldsByGroup('group1')" :key="name"
-                     :name="name"
-                     :field="field" />
-
-            <h3 class="uk-flex uk-flex-middle">
-                <small class="uk-text-muted uk-margin-small-right">{{ 'Stap 2' | trans }}</small>
-                <strong class="uk-flex-1">{{ 'Screenen' | trans }}</strong>
-            </h3>
-            <hr class="uk-margin-small-bottom"/>
-            <Toggler v-for="(field, name) in fieldsByGroup('group2')" :key="name"
-                     :name="name"
-                     :field="field" />
+    <div class="uk-grid-divider uk-grid-small" uk-grid>
+        <div class="uk-width-1-3@s">
+            <ParamsDisplay class="uk-margin"></ParamsDisplay>
         </div>
-        <div>
-            <h3 class="uk-flex uk-flex-middle">
-                <small class="uk-text-muted uk-margin-small-right">{{ 'Stap 3' | trans }}</small>
-                <strong class="uk-flex-1">{{ 'Ranken' | trans }}</strong>
-            </h3>
-            <hr class="uk-margin-small-bottom"/>
-            <Toggler v-for="(field, name) in fieldsByGroup('group3')" :key="name"
-                     :name="name"
-                     :field="field" />
-
-            <h3 class="uk-flex uk-flex-middle">
-                <small class="uk-text-muted uk-margin-small-right">{{ 'Stap 4' | trans }}</small>
-                <strong class="uk-flex-1">{{ 'Sizen' | trans }}</strong>
-            </h3>
-            <hr class="uk-margin-small-bottom"/>
-            <Toggler v-for="(field, name) in fieldsByGroup('group4')" :key="name"
-                     :name="name"
-                     :field="field">
-            </Toggler>
-        </div>
-        <div>
-            <h3 class="uk-flex uk-flex-middle">
-                <small class="uk-text-muted uk-margin-small-right">{{ 'Stap 5' | trans }}</small>
-                <strong class="uk-flex-1">{{ 'Wegen' | trans }}</strong>
-            </h3>
-            <hr class="uk-margin-small-bottom"/>
-            <Toggler v-for="(field, name) in fieldsByGroup('group5')" :key="name"
-                     :name="name"
-                     :field="field" />
-
-            <h3 class="uk-flex uk-flex-middle">
-                <small class="uk-text-muted uk-margin-small-right">{{ 'Stap 6' | trans }}</small>
-                <strong class="uk-flex-1">{{ 'Kosten' | trans }}</strong>
-            </h3>
-            <hr class="uk-margin-small-bottom"/>
-            <Toggler v-for="(field, name) in fieldsByGroup('group6')" :key="name"
-                     :name="name"
-                     :field="field" />
+        <div class="uk-width-2-3@s">
+            <ul uk-accordion="active: 0;" class="uk-accordion uk-accordion-condensed">
+                <li v-for="fieldGroup in groups"
+                    :key="fieldGroup.step">
+                    <a class="uk-accordion-title" href="#">
+                        <small class="uk-text-muted uk-margin-small-right">{{ fieldGroup.step }}</small>
+                        <strong class="uk-flex-1">{{ fieldGroup.label }}</strong>
+                    </a>
+                    <div class="uk-accordion-content">
+                        <Toggler v-for="(field, name) in fieldGroup.fields" :key="name"
+                                 :name="name"
+                                 :field="field" />
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 
 </template>
 <script>
-import {mapState, mapGetters,} from 'vuex';
+    import {mapGetters,} from 'vuex';
 
-export default {
+    export default {
 
-    name: 'ParamsForm',
+        name: 'ParamsForm',
 
-    computed: {
-        ...mapGetters(['fieldsByGroup',]),
-    },
-
-    methods: {
-    },
-}
+        computed: {
+            groups() {
+                return [
+                    {
+                        step: this.$trans('Stap 1'),
+                        label: this.$trans('Data'),
+                        fields: this.fieldsByGroup('group1'),
+                    },
+                    {
+                        step: this.$trans('Stap 2'),
+                        label: this.$trans('Screenen'),
+                        fields: this.fieldsByGroup('group2'),
+                    },
+                    {
+                        step: this.$trans('Stap 3'),
+                        label: this.$trans('Ranken'),
+                        fields: this.fieldsByGroup('group3'),
+                    },
+                    {
+                        step: this.$trans('Stap 4'),
+                        label: this.$trans('Sizen'),
+                        fields: this.fieldsByGroup('group4'),
+                    },
+                    {
+                        step: this.$trans('Stap 5'),
+                        label: this.$trans('Wegen'),
+                        fields: this.fieldsByGroup('group5'),
+                    },
+                    {
+                        step: this.$trans('Stap 6'),
+                        label: this.$trans('Kosten'),
+                        fields: this.fieldsByGroup('group6'),
+                    },
+                ];
+            },
+            ...mapGetters(['fieldsByGroup',]),
+        },
+    }
 
 </script>
 <style lang="less">
