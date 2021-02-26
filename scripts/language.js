b8034d3aa09596a950da72db384e1aeacb1f167b
Matthijs Alles <matthijs@bixie.nl>
2021-02-26 14:41:49 +0100

    improve language

--- scripts/language.js
+++ scripts/language.js
@@ -4,19 +4,51 @@ const path = require('path');
 const {glob,} = require('multi-glob');
 const fs = require('fs');
 
+const langPaths = {
+    'en-GB': path.resolve(__dirname, '../language/en-GB/strings.json'),
+    'nl-NL': path.resolve(__dirname, '../language/nl-NL/strings.json'),
+};
 const strings_enGB = require('../language/en-GB/strings.json');
 const strings_nlNL = require('../language/nl-NL/strings.json');
 
-const existing = {'en-GB': strings_enGB, 'nl-NL': strings_nlNL,};
-const strings = {'en-GB': {}, 'nl-NL': {},};
+const baseStrings = {};
+const existingStrings = {'en-GB': strings_enGB, 'nl-NL': strings_nlNL,};
+const languages = ['en-GB', 'nl-NL',];
 
 const regExVue = /(?:\{\{\s?'(.*)'\s?\|\s?trans\s?\}\})|(?:\$trans\('(.*)'\))/g;
 
-function addString(str) {
+class LanguageStrings {
+    constructor(existing = {}) {
+        this.existing = existing;
+        this.strings = {};
+        this.diffStrings = [];
+    }
+
+    add(key, str) {
+        if (this.existing[key] && this.existing[key] !== str && key !== str) {
+            this.diffStrings.push({key, str, trans: this.existing[key],});
+        }
+        this.strings[key] = this.existing[key] || str;
+        delete this.existing[key];
+    }
+
+    deprecateUnAddedStrings() {
+        Object.entries(this.existing)
+            .forEach(([key, string,]) => {
+                return this.strings[key.startsWith('@deprecated') ? key : `@deprecated.${key}`] =  string;
+            });
+    }
+
+    toString() {
+        return JSON.stringify(this.strings, null, 4);
+    }
+}
+
+function addBaseString(str, key = null) {
     if (!str || duckNumber(str)) {
         return;
     }
-    Object.keys(strings).forEach(locale => strings[locale][str] = existing[locale][str] !== undefined ? existing[locale][str] : str);
+    baseStrings[(key||str)] = str;
 }
 
 function duckNumber(val) {
@@ -30,7 +62,7 @@ function handleVue(file, contents) {
         // console.log(m);
         if (m) {
             let str = m[1] || m[2];
-            addString(str);
+            addBaseString(str);
         }
     } while (m);
 }
@@ -39,12 +71,12 @@ function handleJson(file, contents) {
     const data = JSON.parse(contents);
     if (file === 'data/parameter-fields.json') {
         Object.values(data).forEach(field => {
-            ['info', 'label', 'tip', 'disabled_message',].forEach(prop => addString(field[prop]));
-            field.options.forEach(text => addString(text));
+            ['info', 'label', 'tip', 'disabled_message',].forEach(prop => addBaseString(field[prop]));
+            field.options.forEach(text => addBaseString(text));
         });
     }
     if (file === 'data/gameplans.json') {
-        Object.keys(data).forEach(preset => addString(preset));
+        Object.keys(data).forEach(preset => addBaseString(preset));
     }
 }
 
@@ -56,7 +88,7 @@ glob(['src/**/*.vue', 'data/*.json',], {
         console.trace(er);
         return;
     }
-    [1, 2, 3, 4, 5, 6,].forEach(nr => addString(`pending_message_${nr}`));
+    [1, 2, 3, 4, 5, 6,].forEach(nr => addBaseString(`pending_message_${nr}`));
     files.forEach(file => {
         let contents = fs.readFileSync(file);
         if (file.indexOf('.json') > -1) {
@@ -66,9 +98,15 @@ glob(['src/**/*.vue', 'data/*.json',], {
         }
 
     });
-    Object.keys(strings).forEach(locale => {
-        fs.writeFileSync(path.resolve(__dirname, `../language/${locale}/strings.json`), JSON.stringify(strings[locale], null, 4));
+    //build language files
+    languages.forEach(language => {
+        const langStrings = new LanguageStrings(existingStrings[language]);
+        Object.keys(baseStrings).sort().forEach(key => {
+            langStrings.add(key, baseStrings[key]);
+        });
+        langStrings.deprecateUnAddedStrings();
+        fs.writeFileSync(langPaths[language], langStrings.toString());
+        console.log(`Language ${language} json file saved.`);
     });
-    console.log(strings);
 
 });
